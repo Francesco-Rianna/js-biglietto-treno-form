985c8215e3db3cbaf03391ed4ef2ad1d17f4f526
Francesco-Rianna <maradonino98@hotmail.it>
2024-03-06 19:24:05 +0100

    feat : added milestone2

--- js/script.js
+++ js/script.js
@@ -4,15 +4,13 @@
 
 const generateButton = document.querySelector('#generate-button');
 generateButton.addEventListener('click', function () {
-
-
+    const userName = document.querySelector('#user-name').value;
+    const userLastName = document.querySelector('#user-surname').value;
     const userKm = parseInt(document.querySelector('#user-km').value);
     const userAge = parseInt(document.querySelector('#user-age').value);
 
-
     // Calcolo costo viaggio
     const travelPrice = (userKm * 0.21)
-    console.log(travelPrice)
 
     // Effettuo calcolo in caso di sconti
 
@@ -24,10 +22,65 @@ generateButton.addEventListener('click', function () {
 
 
     }
-    console.log(discount)
+
+    const fullPrice = 'Full Price'
+
+    let offer = fullPrice
+    if (userAge < 18) {
+        offer = '20%'
+    } else if (userAge > 65) {
+        offer = '40%'
+
+
+    }
+
 
     let discountFixed = discount.toFixed(2)
-    console.log(discountFixed)
+
+    // numero posto
+
+    const placeNumber = Math.floor(Math.random() * 10) + 1;
+
+    // numero carrozza
+
+    const codeNumber = Math.floor(Math.random() * 1000) + 1;
+
+    // output per il biglietto
+
+
+    document.querySelector('#name').innerHTML = userName;
+    document.querySelector('#last-name').innerHTML = userLastName;
+    document.querySelector('#discount').innerHTML = offer
+    document.querySelector('#place').innerHTML = placeNumber
+    document.querySelector('#code').innerHTML = codeNumber
+    document.querySelector('#price').innerHTML = discountFixed
+
+    // bottone per annullare
+
+
+
+
+
+
+
+
 })
+
+const deleteButton = document.querySelector('#delete-button');
+deleteButton.addEventListener('click', function () {
+    document.querySelector('#user-name').value = "";
+    document.querySelector('#user-surname').value = "";
+    document.querySelector('#user-km').value = "";
+    document.querySelector('#user-age').value = "";
+
+
+})
+
+
+
+
+
+
+
 
 
